c0d23bd7c2355101e6fc107d648f0bb933651293
Tianhao-Shao <70172849+Tianhao-Shao@users.noreply.github.com>
2023-11-15 12:55:49 -0600

    Finish Ch1-5

--- dashboard/starter-example/app/lib/placeholder-data.js
+++ dashboard/starter-example/app/lib/placeholder-data.js
@@ -3,8 +3,8 @@
 const users = [
   {
     id: '410544b2-4001-4271-9855-fec4b6a6442a',
-    name: 'User',
-    email: 'user@nextmail.com',
+    name: 'tshao',
+    email: 'tshao@uh.edu',
     password: '123456',
   },
 ];
